e214158c248dc7ba954deeba9993f7e4ff11e0ad
Sajid Haider <165992305+sajidhaidersourav@users.noreply.github.com>
2024-10-22 00:41:32 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+// javaScript
+
+// jQuery 
+$(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 993,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      });
+})
